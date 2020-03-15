0b9f921eb31ff910806d88840cd27985f7f1cafa
manoj997 <dharmakumar338@gmail.com>
2020-03-15 10:55:07 +0530

    initial commit to master

---
+++ lib/homescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState() => new _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override 
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Login'),
+      ),
+      body: new Center(
+        child: new Text('This is where the login form would be....'),
+      ),
+    );
+  }
+}
